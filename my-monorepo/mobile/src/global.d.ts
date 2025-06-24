36be67c0b55a1fe88b085b0de09c2f7f47ed7e4a
전선일 <jeonsunil1225@gamil.com>
2025-06-24 11:01:49 +0900

    낮은 버전의 tailwind, nativewind를 설치해서 해보려 했지만 nativewind가 적용이 안되는 문제, -> fabric을 false로 만들었으나, 여전히 적용이 되지 않음.

---
+++ my-monorepo/mobile/src/global.d.ts
@@ -0,0 +1,11 @@
+import { ViewProps, TextProps } from 'react-native';
+
+declare module 'react-native' {
+  interface ViewProps {
+    className?: string;
+  }
+
+  interface TextProps {
+    className?: string;
+  }
+}
---
+++ my-monorepo/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./mobile/src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+console.log('tailwind loaded')
